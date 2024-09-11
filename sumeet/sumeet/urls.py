65a60224737f115b05d69ffee668cb298004b5a0
SumeetShinde1108 <sumeetshinde118@gmail.com>
2024-09-12 00:30:21 +0530

    using hyper links and url tag

--- sumeet/sumeet/urls.py
+++ sumeet/sumeet/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include 
+from course import views
 
 urlpatterns = [
         path('admin/', admin.site.urls),
-        path('cor/',include('course.urls')),
+        path('learndj/',views.learn_django),
+        path('learnpy/',views.learn_python),
+        path('',views.index),
+        path('learnv/',views.learn_variable)
 ]
- 
