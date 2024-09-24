6926faf54b9d367a25c483f277622153d4f2d937
SumeetShinde1108 <sumeetshinde118@gmail.com>
2024-09-24 12:02:54 +0530

    Configuring id attribute,label tag and dynamic initial value

--- Student/registration/forms.py
+++ Student/registration/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Your Name", max_length=100)
-    email = forms.EmailField(label="Your Email")
-    message = forms.CharField(widget=forms.Textarea, label="Your Message")
+    name = forms.CharField()
+    email = forms.EmailField()
+    address=forms.CharField()
