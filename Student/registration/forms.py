8462f0ce4bdc1c452e52a2237b03f17d9abde9de
SumeetShinde1108 <sumeetshinde118@gmail.com>
2024-09-24 20:46:45 +0530

    Rendering form fields manually

--- Student/registration/forms.py
+++ Student/registration/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
-    address=forms.CharField()
+    address=forms.CharField(help_text="In this field we can write 30 characters")
