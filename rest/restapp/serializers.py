f454d55725a94885048292871eaf43fde637b790
SumeetShinde1108 <sumeetshinde118@gmail.com>
2024-10-08 14:00:24 +0530

    deserialization

--- rest/restapp/serializers.py
+++ rest/restapp/serializers.py
@@ -3,3 +3,6 @@ class StudentSerializer(serializers.Serializer):
     name=serializers.CharField(max_length=45)
     roll=serializers.IntegerField()
     city=serializers.CharField(max_length=45)
+
+    def create(self,validate_data):
+        return Student.objects,create(**validate_data)
--- rest/restapp/views.py
+++ rest/restapp/views.py
@@ -1,8 +1,13 @@
+import io
 from django.http import JsonResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render 
 from restapp.models import Student
 from restapp.serializers import StudentSerializer
-from rest_framework.renderers import JSONRenderer  
+from rest_framework.renderers import JSONRenderer 
+from rest_framework.parsers import JSONParser 
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+@csrf_exempt
 def show_data(request, pk):
     stu = Student.objects.get(id=pk)
     serializer = StudentSerializer(stu)
@@ -13,4 +18,16 @@ def show_details(request):
     stu = Student.objects.all()
     serializer = StudentSerializer(stu, many=True)
     return JsonResponse(serializer.data,safe=False)
-        
+
+def create_data(request):
+    json_data=request.body
+    stream=io.BytesIO(json_data)  
+    python_data=JSONParser().parse(stream)
+    serializer=StudentSerializer(python_data)
+    if serializer.is_valid():
+        serialiser.save()
+        res={"msg":"Data created successfully!"}
+        json_data=JSONRenderer().render(res) 
+        return HttpResponse(json_data,content_type='application/json')
+    json_data=JSONRenderer().render(serializer.errors) 
+    return HttpResponse(json_data,content_type='application/json')    
